69fa69a19337b65c457804ce4478bc8aa4bcaab7
RunyShark <spartadragon117@live.com.mx>
2022-10-15 18:31:04 -0500

    flatlist

---
+++ src/screen/Home/HomeStyle.ts
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  componente: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+  },
+  globalMargin: {
+    marginHorizontal: 20,
+  },
+  itemSeparator: {
+    borderBottomWidth: 1,
+    opacity: 0.4,
+    marginVertical: 8,
+  },
+});
--- src/screen/Home/index.tsx
+++ src/screen/Home/index.tsx
@@ -1,10 +1,56 @@
 import React from 'react';
-import {View} from 'react-native';
+import {Text, View} from 'react-native';
+import {FlatList} from 'react-native-gesture-handler';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/Ionicons';
+import {styles} from './HomeStyle';
+interface MenuItem {
+  name: string;
+  icon: string;
+  component: string;
+}
+
+const menuItems: MenuItem[] = [
+  {
+    name: 'Animation 101',
+    icon: 'cube-outline',
+    component: 'Animation101Screen',
+  },
+  {
+    name: 'Animation 102',
+    icon: 'albums-outline',
+    component: 'Animation101Screen',
+  },
+];
+
 export const HomeScreen = () => {
-  return (
+  const {top} = useSafeAreaInsets();
+
+  const renderMenuItem = (menuItem: MenuItem) => (
     <View>
-      <Icon name="star-outline" size={30} color="black" />
+      <Text>
+        {menuItem.name} - {menuItem.icon}
+      </Text>
+    </View>
+  );
+
+  const renderListHeader = () => (
+    <View style={{marginTop: top + 20, marginBottom: 20}}>
+      <Text style={styles.title}>Options menu</Text>
+    </View>
+  );
+
+  const itemSeparator = () => <View style={styles.itemSeparator} />;
+
+  return (
+    <View style={(styles.componente, styles.globalMargin)}>
+      <FlatList
+        data={menuItems}
+        renderItem={({item}) => renderMenuItem(item)}
+        keyExtractor={(item) => item.name}
+        ListHeaderComponent={renderListHeader}
+        ItemSeparatorComponent={itemSeparator}
+      />
     </View>
   );
 };
