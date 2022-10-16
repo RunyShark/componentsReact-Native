efaba8b69537f648c3aeb40d05f63f6f36dbbb64
RunyShark <spartadragon117@live.com.mx>
2022-10-16 12:28:11 -0500

    modal

--- src/screen/Modal/index.tsx
+++ src/screen/Modal/index.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Alert, Modal, Text, Pressable as Presumable, View} from 'react-native';
+import {styles} from './ModalStyle';
 
 export const ModalScreen = () => {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
-    <View>
-      <Text>ModalScreen</Text>
+    <View style={styles.centeredView}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Presumable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Presumable>
+          </View>
+        </View>
+      </Modal>
+      <Presumable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(true)}>
+        <Text style={styles.textStyle}>Show Modal</Text>
+      </Presumable>
     </View>
   );
 };
