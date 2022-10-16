cf93ee976cafbfc9a7425354aa0a54b2f3ba3459
RunyShark <spartadragon117@live.com.mx>
2022-10-16 14:44:17 -0500

    slice

--- src/screen/Slice/SliceStyle.ts
+++ src/screen/Slice/SliceStyle.ts
@@ -1,9 +1,17 @@
 import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColo: 'red',
-    paddingTop: 50,
+  imageStyle: {
+    width: 350,
+    height: 400,
+    resizeMode: 'center',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#5856D6',
+  },
+  subtitle: {
+    fontSize: 16,
   },
 });
--- src/screen/Slice/index.tsx
+++ src/screen/Slice/index.tsx
@@ -1,20 +1,37 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import Carousel from 'react-native-snap-carousel';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {HeaderTitle, items, Slide} from '../../index';
-import {Dimensions, Text} from 'react-native';
+import {items, Slide} from '../../index';
+import {Dimensions, Image, Text, View} from 'react-native';
+import {styles} from './SliceStyle';
 
 const {width: screenWidth} = Dimensions.get('window');
 export const SliceScreen = () => {
-  const renderItem = ({desc}: Slide) => <Text>{desc}</Text>;
+  const renderItem = ({desc, img, title}: Slide) => (
+    <View
+      style={{
+        flex: 1,
+        borderRadius: 5,
+        backgroundColor: 'white',
+        padding: 80,
+      }}>
+      <Image source={img} style={styles.imageStyle} />
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.subtitle}>{desc}</Text>
+    </View>
+  );
   return (
-    <SafeAreaView>
-      <HeaderTitle title="Slice" />
+    <SafeAreaView
+      style={{
+        flex: 1,
+      }}>
       <Carousel
         data={items}
         renderItem={({item}: {item: Slide}) => renderItem(item)}
         sliderWidth={screenWidth}
         itemWidth={screenWidth}
+        layout="default"
       />
     </SafeAreaView>
   );
