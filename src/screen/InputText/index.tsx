639726b8578ee636c64c02cde2997283be36f69d
RunyShark <spartadragon117@live.com.mx>
2022-10-16 11:48:58 -0500

    textInput

--- src/screen/InputText/index.tsx
+++ src/screen/InputText/index.tsx
@@ -1,8 +1,17 @@
 import React, {useState} from 'react';
-import {TextInput, KeyboardAvoidingView, Platform, View} from 'react-native';
+import {
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  Keyboard,
+} from 'react-native';
 import {styles} from './InputTextStyle';
 import {HeaderTitle} from '../../index';
-import {ScrollView} from 'react-native-gesture-handler';
+import {
+  ScrollView,
+  TouchableWithoutFeedback,
+} from 'react-native-gesture-handler';
 
 interface stateProps {
   name: string;
@@ -30,39 +39,41 @@ export const InputTextScreen = () => {
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
       <HeaderTitle title={'Input Text'} />
       <ScrollView>
-        <TextInput
-          style={styles.input}
-          onChangeText={(value) => onChange(value, 'name')}
-          value={name}
-          placeholder="Name"
-          autoCorrect={false}
-          autoCapitalize="words"
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={(value) => onChange(value, 'lastName')}
-          value={lastName}
-          placeholder="LastName"
-          autoCorrect={false}
-          autoCapitalize="words"
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={(value) => onChange(value, 'direction')}
-          value={direction}
-          placeholder="direction"
-          autoCapitalize="none"
-          keyboardType="email-address"
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={(value) => onChange(value, 'phone')}
-          value={phone}
-          placeholder="Phone"
-          autoCapitalize="none"
-          keyboardType="phone-pad"
-        />
-        <View style={styles.viewStyle} />
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <TextInput
+            style={styles.input}
+            onChangeText={(value) => onChange(value, 'name')}
+            value={name}
+            placeholder="Name"
+            autoCorrect={false}
+            autoCapitalize="words"
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={(value) => onChange(value, 'lastName')}
+            value={lastName}
+            placeholder="LastName"
+            autoCorrect={false}
+            autoCapitalize="words"
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={(value) => onChange(value, 'direction')}
+            value={direction}
+            placeholder="direction"
+            autoCapitalize="none"
+            keyboardType="email-address"
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={(value) => onChange(value, 'phone')}
+            value={phone}
+            placeholder="Phone"
+            autoCapitalize="none"
+            keyboardType="phone-pad"
+          />
+          <View style={styles.viewStyle} />
+        </TouchableWithoutFeedback>
       </ScrollView>
     </KeyboardAvoidingView>
   );
