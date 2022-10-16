4208a544febb905a9dfe04dd2ea89fdaccb866e4
RunyShark <spartadragon117@live.com.mx>
2022-10-15 19:27:18 -0500

    animate

--- src/screen/animation/Animation01Screen.tsx
+++ src/screen/animation/Animation01Screen.tsx
@@ -1,9 +1,10 @@
 import React, {useRef} from 'react';
-import {Animated, Button, View} from 'react-native';
+import {Animated, Button, View, Easing} from 'react-native';
 import {styles} from './AnimationStyle';
 
 export const Animation01Screen = () => {
-  const opacity = useRef(new Animated.Value(0.4)).current;
+  const opacity = useRef(new Animated.Value(0)).current;
+  const top = useRef(new Animated.Value(-100)).current;
 
   const fadeIn = () => {
     Animated.timing(opacity, {
@@ -11,11 +12,18 @@ export const Animation01Screen = () => {
       duration: 300,
       useNativeDriver: true,
     }).start();
+
+    Animated.timing(top, {
+      toValue: 0,
+      duration: 700,
+      useNativeDriver: true,
+      easing: Easing.bounce,
+    }).start();
   };
 
   const fadeOut = () => {
     Animated.timing(opacity, {
-      toValue: 0.4,
+      toValue: 0,
       duration: 300,
       useNativeDriver: true,
     }).start();
@@ -28,7 +36,8 @@ export const Animation01Screen = () => {
         style={{
           ...styles.purpleBox,
           marginBottom: 20,
-          opacity: opacity,
+          opacity,
+          transform: [{translateY: top}],
         }}
       />
       <Button title="FadeIn" onPress={fadeIn} />
