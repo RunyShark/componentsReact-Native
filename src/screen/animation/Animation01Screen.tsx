4e8b000595f55ebf036abcd4d37037bd3b9dab1b
RunyShark <spartadragon117@live.com.mx>
2022-10-15 19:18:28 -0500

    animate

--- src/screen/animation/Animation01Screen.tsx
+++ src/screen/animation/Animation01Screen.tsx
@@ -1,18 +1,38 @@
 import React, {useRef} from 'react';
-import {Animated, View} from 'react-native';
+import {Animated, Button, View} from 'react-native';
 import {styles} from './AnimationStyle';
 
 export const Animation01Screen = () => {
   const opacity = useRef(new Animated.Value(0.4)).current;
 
+  const fadeIn = () => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const fadeOut = () => {
+    Animated.timing(opacity, {
+      toValue: 0.4,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
     <View style={styles.animationOneContainer}>
       <Animated.View
+        // eslint-disable-next-line react-native/no-inline-styles
         style={{
           ...styles.purpleBox,
+          marginBottom: 20,
           opacity: opacity,
         }}
       />
+      <Button title="FadeIn" onPress={fadeIn} />
+      <Button title="fadeOut" onPress={fadeOut} />
     </View>
   );
 };
